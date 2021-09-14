aff0d81429b2ddcdcb3efbf1567c7f0dbddb2d57
karthihapriya <karthihapriya@gmail.com>
2021-09-14 15:12:48 +0530

    adding the components from project A from checkpoint

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from "./screens/home/Home";
+import Details from "./screens/details/Details";
+import './index.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NotFound from './common/NotFound';
+import 'typeface-roboto';
+
+
+ReactDOM.render(
+    <BrowserRouter>
+      {/* <Home /> */}
+      {/* <Details /> */}
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/detail" component={Details}/>
+        <Route component={NotFound}/>
+      </Switch>
+    </BrowserRouter>
+,
+  document.getElementById('root')
+);
